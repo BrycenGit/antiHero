b160bf1822e7bc5af61cf1aa49a2f1c7ef381792
Brycen Bartolome <brycenbartolome@gmail.com>
2021-01-22 00:14:13 -0800

    function does damage to character and opposite, function to check health

--- src/components/rpg.js
+++ src/components/rpg.js
@@ -1,35 +1,65 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 const Hero = {
   name: "hero",
   health: 100,
   exp: 0,
   type: "water",
+  attack: 20,
 };
 
-const Enemy = {
-  name: "hero",
+const antiHero = {
+  name: "anti-hero",
   health: 100,
   exp: 0,
   type: "water",
+  attack: 20,
 };
 const Rpg = () => {
   const [character, setCharacter] = useState(Hero);
-  const [enemy, setEnemy] = useState(Enemy);
+  const [enemy, setEnemy] = useState(antiHero);
 
-  const handleDamage = () => {
-    setCharacter({ ...character, health: character.health - 10 });
-    console.log(character);
+  const checkHealth = (player) => {
+    if (player.health <= 0) {
+      alert(`${player.name} lost`);
+    }
+  };
+
+  const characterReceiveDamage = async () => {
+    setCharacter({ ...character, health: character.health - enemy.attack });
+    await checkHealth(character);
+  };
+
+  const enemyReceiveDamage = () => {
+    setEnemy({ ...enemy, health: enemy.health - character.attack });
+    checkHealth(enemy);
   };
 
   return (
-    <div>
+    <>
       <div>Rpg</div>
-      <div>Character: {character.name}</div>
-      <div>Health: {character.health}</div>
-      <button onClick={handleDamage}>Damage</button>
-    </div>
+      <Box>
+        <div className="hero">
+          <div>Character: {character.name}</div>
+          <div>Health: {character.health}</div>
+          <button onClick={enemyReceiveDamage}>Damage</button>
+        </div>
+        <div className="enemy">
+          <div>Enemy: {enemy.name}</div>
+          <div>Health: {enemy.health}</div>
+          <button onClick={characterReceiveDamage}>Damage</button>
+        </div>
+      </Box>
+    </>
   );
 };
 
 export default Rpg;
+const Box = styled.div`
+  margin: 80px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
